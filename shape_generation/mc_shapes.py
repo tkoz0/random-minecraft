f30e9d4868306dbeb6923d6b0d9ecdb398378a20
TKoz Zero <tkozlowski1999@gmail.com>
2021-05-17 06:12:35 -0400

    added open circles

--- shape_generation/mc_shapes.py
+++ shape_generation/mc_shapes.py
@@ -4,7 +4,7 @@ center at block center. Use block centers to calculate distance for determining
 if it is included as inside the circle.
 '''
 
-def circle(d):
+def filled_circle(d):
     assert type(d) == int and d > 0
     if d % 2 == 0: # even, centered at block corner
         R = d//2
@@ -29,14 +29,55 @@ def circle(d):
         H = [row[1:][::-1] + row for row in Q] # mirror horizontally
         return [row[:] for row in H[1:][::-1]] + H
 
+def open_circle(d):
+    # similar to filled circle, but marks the edges by calculating
+    # the largest value satisfying the equation
+    assert type(d) == int and d > 0
+    if d % 2 == 0:
+        R = d//2
+        Q = [[False for c in range(R)] for r in range(R)] # empty
+        for i in range(R):
+            # by symmetry, let r=i, then find maximum c
+            # i**2 + i + c**2 + c < R**2
+            # this can be rearranged to c**2 + c < R**2 - i**2 - i
+            rhs = R*R - i*i - i
+            c = 0
+            while c**2 + c < rhs: c += 1
+            c -= 1
+            Q[i][c] = True
+            Q[c][i] = True
+        # same as above
+        H = [row[::-1] + row for row in Q] # mirror horizontally
+        return [row[:] for row in H[::-1]] + H
+    else:
+        R = (d+1)//2
+        Q = [[False for c in range(R)] for r in range(R)] # empty
+        for i in range(R):
+            # by symmetry, let r=i, then find maximum c
+            # r**2 + c**2 <= R**2 - R
+            rhs = R*R - R - i*i
+            c = 0
+            while c*c <= rhs: c += 1
+            c -= 1
+            Q[i][c] = True
+            Q[c][i] = True
+        # same as above
+        H = [row[1:][::-1] + row for row in Q] # mirror horizontally
+        return [row[:] for row in H[1:][::-1]] + H
+
 if __name__ == '__main__':
     import sys
     import png
-    C = circle(int(sys.argv[1]))
-    if len(sys.argv) > 2:
+    if sys.argv[2] == 'open':
+        C = open_circle(int(sys.argv[1]))
+    elif sys.argv[2] == 'filled':
+        C = filled_circle(int(sys.argv[1]))
+    else:
+        assert 0
+    if len(sys.argv) > 3:
         arr = [[0 if b else 255 for b in row] for row in C]
         img = png.from_array(arr,'L;8')
-        img.save(sys.argv[2])
+        img.save(sys.argv[3])
     else:
         for row in C:
             print(''.join('X' if b else ' ' for b in row))
